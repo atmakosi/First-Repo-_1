5eeaef4b87803c0704739c91e298aad95ad2db8a
Anathi Makosi <atmakosi@gmail.com>
2026-03-13 20:29:35 +0200

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,12 @@
+float_var= 5.67
+print(float_var)
+inte_var = 5
+print(inte_var)
+string_var = "Busie"
+print(string_var)   
+converted_float = float(inte_var)
+print(converted_float)  
+converted_int= int(float_var)
+print(converted_int)
+converted_string = str(inte_var)
+print(converted_string) 
---
+++ prompt variables.py
@@ -0,0 +1,7 @@
+user=("name","age","house","street name")
+print(user)
+name="Anathi"
+age=25
+house="1234"    
+street_name="lasco"
+print(name,age,house,street_name)
